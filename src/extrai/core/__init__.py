0302d01ce4015013877dd5a1d428c496046b6f04
Telsho <ethouant-ext@eutelsat.com>
2026-01-17 18:06:08 +0100

    fix: cleaning + imports

--- src/extrai/core/__init__.py
+++ src/extrai/core/__init__.py
@@ -20,15 +20,11 @@ from .errors import (
 
 from .base_llm_client import BaseLLMClient
 from .analytics_collector import WorkflowAnalyticsCollector
-from .json_consensus import JSONConsensus, default_conflict_resolver
+from .json_consensus import JSONConsensus, default_conflict_resolver, prefer_most_common_resolver, SimilarityClusterResolver
 from .prompt_builder import generate_system_prompt, generate_user_prompt_for_docs
-from .schema_inspector import (
-    generate_llm_schema_from_models,
-    discover_sqlmodels_from_root,
-    inspect_sqlalchemy_model,
-)
-from .sqlalchemy_hydrator import SQLAlchemyHydrator
-from .db_writer import persist_objects
+from .model_registry import ModelRegistry
+from .schema_inspector import SchemaInspector
+from .result_processor import ResultProcessor, SQLAlchemyHydrator, persist_objects
 from .workflow_orchestrator import WorkflowOrchestrator
 from .sqlmodel_generator import SQLModelCodeGenerator
 from .example_json_generator import ExampleJSONGenerator
@@ -50,14 +46,16 @@ __all__ = [
     "WorkflowAnalyticsCollector",
     "JSONConsensus",
     "default_conflict_resolver",
+    "prefer_most_common_resolver",
     "generate_system_prompt",
     "generate_user_prompt_for_docs",
-    "generate_llm_schema_from_models",
-    "discover_sqlmodels_from_root",
-    "inspect_sqlalchemy_model",
+    "ModelRegistry",
+    "SchemaInspector",
+    "ResultProcessor",
     "SQLAlchemyHydrator",
     "persist_objects",
     "WorkflowOrchestrator",
     "SQLModelCodeGenerator",
     "ExampleJSONGenerator",
+    "SimilarityClusterResolver"
 ]
